9290365f0aae27e73170b626813fc250b8edc885
Mia Laurea <mlaurea@mltc02pt29qfvh7.bkprecision.local>
2018-07-12 08:51:44 -0700

    saving files again before merging to master

--- src/App.js
+++ src/App.js
@@ -13,16 +13,14 @@ class App extends Component{
 
     this.state = {
       todos: [
-        {id: 0, text: "make dinner tonight!" },
-        {id: 1, text: "blah"},
-        {id: 2, text: "boo"}
+        {id: 0, text: "clean up code" },
+        {id: 1, text: "push to Heroku"},
+        {id: 2, text: "create new components"}
       ],
       nextId: 3
     }
-
     this.addTodo = this.addTodo.bind(this);
     this.removeTodo = this.removeTodo.bind(this);
-
   }
 
 addTodo(todoText) {
@@ -35,42 +33,15 @@ addTodo(todoText) {
 }
 
 removeTodo(id) {
-this.setState({
-  todos: this.state.todos.filter((todo, index) => todo.id !== id)
-
-});
-}
-
-handleSubmit = (newNew) => {
-  console.log('was clicked');
-
-  this.refs.project.value= null;
-  this.refs.lead.value = null;
-  this.refs.date.value = null;
-}
-
+    this.setState({
+        todos: this.state.todos.filter((todo, index) => todo.id !== id)
+      });
+    }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherent',
-      border: '1px solid #eee',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
-
     return (
       <div className="App">
       <h1> BK manage prototype</h1>
-      <p> Header (are links necessary?) </p>
-
-      <input ref = 'project' type = 'text' placeholder='project name' />
-      <input ref = 'lead' type = 'text' placeholder = 'project leader'/>
-      <input ref = 'date' type = 'date' />
-      <button onClick ={this.handleSubmit}> Submit </button>
-
-
       <div className = 'project-wrapper'>
           <TodoInput todoText= '' addTodo = {this.addTodo}/>
           <ul> {
@@ -80,8 +51,6 @@ handleSubmit = (newNew) => {
           }
           </ul>
       </div>
-
-
       </div>
     );
   }
