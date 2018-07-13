154c26cadecde0e23890b897ca879802a2354852
Renato Araga <admin@bkprecisioninstruments.com>
2018-07-13 10:16:20 -0700

    General clean up, component is isolated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import './Todo/todoItem.css';
-import './Components/table.css';
-import NavBar from './Components/navbar'
-import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
-
 
 let table_info =
 {
@@ -16,37 +11,32 @@ let table_info =
 };
 
 class Tabletest extends Component {
-  constructor(props){
-    super(props);
-  }
   render() {
       return (
     <div className = "table-container" >
-        <h5> Projects </h5>
-    <table>
-      <thead>
-        <tr>
-          <th> Project name </th>
-          <th> Status </th>
-          <th> Pending Overview </th>
-          <th> Date Started</th>
-        </tr>
-      </thead>
-        <tbody>
-            <tr>
-              <td> {this.props.name} </td>
-              <td> {this.props.status}</td>
-              <td> test data  </td>
-              <td>  </td>
-            </tr>
-        </tbody>
-      </table>
+        <h5>Projects</h5>
+      <table>
+        <thead>
+          <tr>
+            <th>Project name</th>
+            <th>Status</th>
+            <th>Pending Overview</th>
+            <th>Date Started</th>
+          </tr>
+        </thead>
+          <tbody>
+              <tr>
+                <td>{this.props.name}</td>
+                <td>{this.props.status}</td>
+                <td>test data</td>
+                <td>test data</td>
+              </tr>
+          </tbody>
+        </table>
       </div>
       );
   }
 }
-
-
 class App extends Component{
   constructor(){
     super();
@@ -59,12 +49,9 @@ class App extends Component{
   render() {
     return (
       <div className="App">
-      <NavBar/>
-      <h1> New Product Introduction Tool </h1>
-      <Tabletest projectItem = {this.state.tableData && this.state.tableData.projectItem}/>
-
+      <h1>New Product Introduction Tool</h1>
+        <Tabletest projectItem = {this.state.tableData && this.state.tableData.projectItem}/>
       </div>
-
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
