051208f5d75f275ee7feffe1c64f5b052c95ba52
Mia Laurea <mlaurea@mltc02pt29qfvh7.bkprecision.local>
2018-07-19 12:44:45 -0700

    created  an input form for the appending projects button

--- src/components/button.js
+++ src/components/button.js
@@ -1,17 +1,45 @@
 import React from 'react';
+import '../styles/button.css'
 
 class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: '',
+      name: '',
+      date: ''
+    };
 
-  handleClick() {
-    console.log('this is: ', this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(event) {
+    this.setState({
+      [event.target.name]:event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A input was submitted: ' + this.state.product + ', ' + this.state.name +', '+ this.state.date);
+    event.preventDefault();
+  }
 
 render(){
     return(
-      <button onClick ={(e) => this.handleClick(e)}>
-        Hello Create a project test
-      </button>
+      <div className ="add-button">
+      <form onSubmit={this.handleSubmit} >
+      <label>
+        Product:
+        <input type="text" value={this.state.value} name= "product" onChange={this.handleChange}/>
+        Name:
+        <input type="text" value={this.state.name} name="name" onChange={this.handleChange}/>
+        Date:
+        <input type="text" value={this.state.date} name="date" onChange={this.handleChange} />
+      </label>
+      <input type="submit" value="create a project" />
+      </form>
+      </div>
     );
   }
 }
