d8cc9c89222297b249aebbf71ddc4083c5d74ba4
Renato Araga <admin@bkprecisioninstruments.com>
2018-08-01 13:47:04 -0700

    Added Tab component into content container

--- src/components/Projectdetails.js
+++ src/components/Projectdetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Quickview from '../components/Quickview';
+import Window from '../components/tab'
 
 const Projectdetails = () => {
   return (
@@ -9,6 +10,9 @@ const Projectdetails = () => {
         <h3 className="titleDefault"><NavLink to="/">ManageBK</NavLink></h3></div>
       <div className="contentDefault">
         <Quickview/>
+      </div>
+      <div className="contentDefault">
+        <Window/>
       </div>
     </div>
 
