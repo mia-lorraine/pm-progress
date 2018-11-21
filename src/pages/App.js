83065a3f89a822d58aa5fd4b74c5541b477346e2
Mia Laurea <mlaurea@mltc02pt29qfvh7.bkprecision.local>
2018-11-21 14:17:01 -0800

    Able to route to PricingFrom component

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../components/Home';
 import Error from '../components/Error';
 import Addproject from '../components/Addproject';
-import Projectdetails from '../components/Projectdetails'
+import Projectdetails from '../components/Projectdetails';
+import PricingForm from '../components/tabs/PricingForm';
 
 class App extends Component{
   render() {
@@ -12,6 +13,7 @@ class App extends Component{
           <Switch>
             <Route path="/" component={Home} exact/>
             <Route path="/add" component={Addproject} />
+            <Route path="/pricing" component={PricingForm} />
             <Route path="/project-details" component={Projectdetails} />
             <Route component={Error} />
           </Switch>
