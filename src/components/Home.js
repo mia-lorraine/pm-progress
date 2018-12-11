d238ce1c9342acfb0639962ff22cb219315dc94a
Renato Araga <admin@bkprecisioninstruments.com>
2018-12-11 14:21:54 -0800

    Master Rebuild

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import '../styles/App.css';
-import { Grid, Table, ProgressBar } from 'react-bootstrap';
+import { Grid, Table } from 'react-bootstrap';
 import Details from '../components/Details';
 import Navigation from '../components/Navigation';
 
@@ -67,78 +67,7 @@ class Home extends React.Component {
     this.getPricing();
   }
 
-  render() { 
-    let count1 = this.state.projects.filter(finder => finder.p_requestFromSupplier01a === true).length
-    let count2 = this.state.projects.filter(finder => finder.p_requestFromSupplier01b === true).length
-    let count3 = this.state.projects.filter(finder => finder.p_requestFromSupplier01c === true).length
-    let count4 = this.state.projects.filter(finder => finder.p_requestFromSupplier02a === true).length
-    let count5 = this.state.projects.filter(finder => finder.p_requestFromSupplier02b === true).length
-    let count6 = this.state.projects.filter(finder => finder.p_requestFromSupplier02c === true).length
-    let count7 = this.state.projects.filter(finder => finder.p_requestFromSupplier03a === true).length
-    let count8 = this.state.projects.filter(finder => finder.p_requestFromSupplier03b === true).length
-    let count9 = this.state.projects.filter(finder => finder.p_requestFromSupplier03c === true).length
-    let count10 = this.state.projects.filter(finder => finder.p_requestFromSupplier04a === true).length
-    let count11 = this.state.projects.filter(finder => finder.p_requestFromSupplier04b === true).length
-    let count12 = this.state.projects.filter(finder => finder.p_requestFromSupplier04c === true).length
-    let count13 = this.state.projects.filter(finder => finder.p_requestFromSupplier05a === true).length
-    let count14 = this.state.projects.filter(finder => finder.p_requestFromSupplier05b === true).length
-    let count15 = this.state.projects.filter(finder => finder.p_requestFromSupplier05c === true).length
-    let count16 = this.state.projects.filter(finder => finder.p_requestFromSupplier06a === true).length
-    let count17 = this.state.projects.filter(finder => finder.p_requestFromSupplier06b === true).length
-    let count18 = this.state.projects.filter(finder => finder.p_requestFromSupplier06c === true).length
-    let count19 = this.state.projects.filter(finder => finder.p_requestFromSupplier07a === true).length
-    let count20 = this.state.projects.filter(finder => finder.p_requestFromSupplier07b === true).length
-    let count21 = this.state.projects.filter(finder => finder.p_requestFromSupplier07c === true).length
-    let count22 = this.state.projects.filter(finder => finder.p_requestFromSupplier08a === true).length
-    let count23 = this.state.projects.filter(finder => finder.p_requestFromSupplier08b === true).length
-    let count24 = this.state.projects.filter(finder => finder.p_requestFromSupplier08c === true).length
-    let count25 = this.state.projects.filter(finder => finder.p_requestFromSupplier09a === true).length
-    let count26 = this.state.projects.filter(finder => finder.p_requestFromSupplier09b === true).length
-    let count27 = this.state.projects.filter(finder => finder.p_requestFromSupplier09c === true).length
-    let count28 = this.state.projects.filter(finder => finder.p_requestFromSupplier10a === true).length
-    let count29 = this.state.projects.filter(finder => finder.p_requestFromSupplier10b === true).length
-    let count30 = this.state.projects.filter(finder => finder.p_requestFromSupplier10c === true).length
-    let count31 = this.state.projects.filter(finder => finder.p_requestFromSupplier11a === true).length
-    let count32 = this.state.projects.filter(finder => finder.p_requestFromSupplier11b === true).length
-    let count33 = this.state.projects.filter(finder => finder.p_requestFromSupplier11c === true).length
-
-var total = (count1
-+count2
-+count3
-+count4
-+count5
-+count6
-+count7
-+count8
-+count9
-+count10
-+count11
-+count13
-+count14
-+count15
-+count16
-+count17
-+count18
-+count19
-+count20
-+count21
-+count22
-+count23
-+count24
-+count25
-+count26
-+count27
-+count28
-+count29
-+count30
-+count31
-+count32
-+count33 )
-  
-var requestTotal = console.log (total)
-  
-  
-
+  render() {
     return (
       <div className="App">
         <Navigation />
@@ -152,7 +81,6 @@ var requestTotal = console.log (total)
                     <th className="headerStyle">Product</th>
                     <th className="headerStyle">Manager</th>
                     <th className="headerStyle">Date</th>
-                    <th className="headerStyle">Progress</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -165,7 +93,6 @@ var requestTotal = console.log (total)
                       </td>
                       <td>{item.manager}</td>
                       <td>{item.date}</td>  
-                      <td> <ProgressBar bsStyle = "success" now ={requestTotal}></ProgressBar></td>
                     </tr>
                   ))}
                 </tbody>
